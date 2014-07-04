62e97dda54833178876aa70b856d052c65237d99
dottie333 <dorothyp533@gmail.com>
2014-07-03 23:21:36 -0500

    more simple functions

--- inverse_matrix_numpy.py
+++ inverse_matrix_numpy.py
@@ -12,14 +12,15 @@ def inverse_matrix(data):
     
     return data.T
 
-
-print inverse_matrix([[1, 2, 3],
-                      [4, 5, 6],
-                      [7, 8, 9]]) 
-print inverse_matrix([[1, 4, 3],
-                      [8, 2, 6],
-                      [7, 8, 3],
-                      [4, 9, 6],
-                      [7, 8, 1]])
+if __name__ == '__main__':
+    
+    print inverse_matrix([[1, 2, 3],
+                          [4, 5, 6],
+                          [7, 8, 9]]) 
+    print inverse_matrix([[1, 4, 3],
+                          [8, 2, 6],
+                          [7, 8, 3],
+                          [4, 9, 6],
+                          [7, 8, 1]])
 
 
